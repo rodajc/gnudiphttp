2f3299a166814e9a2641d2fbe506cef37dd4af4a
rodajc <github@up.nl.eu.org>
2020-09-18 00:06:14 +0200

    Add files via upload

---
+++ gnudiphttp.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# Register current IP
+# Refer to: http://gnudip2.sourceforge.net/gnudip-www/latest/gnudip/html/protocol.html
+
+TMP1=/tmp/gnudip1.html
+TMP2=/tmp/gnudip2.html
+
+URLBASE="http://gnudip.dynu.com/gnudip"
+
+wget -qO $TMP1 $URLBASE
+
+salt=$(sed -n '/^<meta name="salt"/s/.*content="\(.*\)">/\1/p' $TMP1)
+time=$(sed -n '/^<meta name="time"/s/.*content="\(.*\)">/\1/p' $TMP1)
+sign=$(sed -n '/^<meta name="sign"/s/.*content="\(.*\)">/\1/p' $TMP1)
+user="**********"
+pass="**********"
+pass=$(echo -n "$(echo -n $pass | md5sum | sed 's, .*,,').$salt" | md5sum | sed 's, .*,,')
+domn="box.dynu.com"
+reqc="2"
+
+URL="$URLBASE?salt=$salt&time=$time&sign=$sign&user=$user&pass=$pass&domn=$domn&reqc=$reqc"
+
+wget -qO $TMP2 $URL
+
+if grep -q '^<meta name="retc"' $TMP2; then
+	exit $(sed -n '/<meta name="retc"/s/.*content="\(.*\)">/\1/p' $TMP2)
+else
+	exit 1
+fi
+
